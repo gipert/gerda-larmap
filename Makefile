8b8089addd4de0769067bb94c61db76847bfbec1
Luigi Pertoldi <luigi.pertoldi@hotmail.it>
2019-03-24 18:37:45 +0100

    [doctor] add sanity checking tools

--- Makefile
+++ Makefile
@@ -2,15 +2,22 @@
 #
 # Author: Luigi Pertoldi - pertoldi@pd.infn.it
 # Created: Sun 24 Mar 2019
+#
+FLAGS  = -std=c++11 -g -O3 -Wall
+FLAGS += $$(root-config --libs --cflags) -lTreePlayer
+FLAGS += $$(gerda-ada-config --libs --cflags)
+FLAGS += $$(gelatio-config --libs --cflags)
+FLAGS += $$(mgdo-config --libs --cflags)
+FLAGS += $$(databricxx-config --libs --cflags)
+FLAGS += -Iprogressbar
+
+all : create-larmap larmap-doctor
 
 create-larmap : create-larmap.cc progressbar/ProgressBar.cc
-	c++ -std=c++11 -g -O3 -Wall -o $@ $^             \
-        $$(root-config --libs --cflags) -lTreePlayer \
-        $$(gerda-ada-config --libs --cflags)         \
-        $$(gelatio-config --libs --cflags)           \
-        $$(mgdo-config --libs --cflags)              \
-        $$(databricxx-config --libs --cflags)        \
-        -Iprogressbar
+	c++ $(FLAGS) -o $@ $^
+
+larmap-doctor : larmap-doctor.cc progressbar/ProgressBar.cc
+	c++ $(FLAGS) -o $@ $^
 
 clean :
-	-rm create-larmap
+	-rm create-larmap larmap-doctor
---
+++ larmap-doctor.cc
@@ -0,0 +1,51 @@
+/* larmap-doctor.cc
+ *
+ * Health check of GERDA LAr probability maps.
+ *
+ * Author: Luigi Pertoldi - pertoldi@pd.infn.it
+ * Created: Sun 24 Mar 2019
+ */
+
+// STL
+#include <iostream>
+
+// ROOT
+#include "TFile.h"
+#include "TH3D.h"
+
+// other
+#include "ProgressBar.h"
+
+int main(int argc, char** argv) {
+
+    auto filename = argc > 1 ? argv[1] : "gerda-larmap.root";
+    std::cout << "INFO: opening " << filename << std::endl;
+    TFile f(filename, "read");
+    auto h = dynamic_cast<TH3D*>(f.Get("LAr_prob_map"));
+
+    int missing = 0;
+    int big_error = 0;
+    int ncells = h->GetNcells();
+
+    ProgressBar bar(ncells); std::cout << "INFO: ";
+    for (int i = 0; i < ncells; ++i) {
+        if (h->GetBinContent(i) < 0) missing += 1;
+        if (h->GetBinError(i) > 0.01*h->GetBinContent(i)) big_error += 1;
+        bar.Update();
+    }
+    std::cout << std::endl;
+
+    if (missing) {
+        std::cerr << "WARNING: " <<  missing << "/" << ncells << "("
+                  << round(missing*1000./ncells)/10 << "%)"
+                  << "invalid (<0) voxels found\n";
+    }
+    if (big_error) {
+        std::cerr << "WARNING: " << big_error << "/" << ncells << " ("
+                  << round(big_error*1000./ncells)/10
+                  << "%) voxels with uncertainty > 1% found\n";
+    }
+
+    if (missing || big_error) return 1;
+    else                      return 0;
+}
