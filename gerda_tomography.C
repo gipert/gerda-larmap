a425aa9f099f6020a55fe766d820fa6109eb3a78
Luigi Pertoldi <luigi.pertoldi@hotmail.it>
2019-03-25 12:06:49 +0100

    [tac] fixes to gerda_tomography

--- gerda_tomography.C
+++ gerda_tomography.C
@@ -30,22 +30,29 @@ void gerda_tomography() {
     TFile* tfile = new TFile(filename.c_str(), "read");
     auto LAr_prob_map = dynamic_cast<TH3D*>(tfile->Get("LAr_prob_map"));
 
-    LAr_prob_map->RebinX(2);
-    LAr_prob_map->RebinY(2);
-    LAr_prob_map->RebinZ(2);
+    std::cout << "INFO: preparing histogram...\n";
+    // this is needed for a meaningful rebin...
+    for (int i = 0; i < LAr_prob_map->GetNcells(); ++i) {
+        if (LAr_prob_map->GetBinContent(i) < 0) LAr_prob_map->SetBinContent(i, 0);
+    }
+    auto factor = 2;
+    LAr_prob_map->Rebin3D(factor, factor, factor);
+    LAr_prob_map->Scale(1./(factor*factor*factor));
 
     std::cout << "INFO: displaying...\n";
     TCanvas * c = new TCanvas(
         "gerda_tomography", "GERDA Tomography",
-        600, 1200
+        600, 1000
     );
     c->SetMargin(0, 0, 0, 0);
 
     LAr_prob_map->SetTitle("LAr photon detection probability map");
     LAr_prob_map->SetStats(false);
-    LAr_prob_map->Draw("a box");
+    LAr_prob_map->SetMinimum(0);
+    LAr_prob_map->SetMaximum(1);
+    LAr_prob_map->Draw("a fb bb lego");
 
-    LAr_prob_map->SetShowProjection("xy col");
+    LAr_prob_map->SetShowProjection("xy colz");
     gPad->SetTitle("GERDA Tomography (projection)");
 
     return;
